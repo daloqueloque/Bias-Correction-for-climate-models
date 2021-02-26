a77c77fdec1d68ea9c24466c57b2d39f29c41f84
Dani Loqueloque <79607002+DarkLynx56@users.noreply.github.com>
2021-02-26 20:43:31 +0800

    Update MBCn.py

--- MBCn.py
+++ MBCn.py
@@ -6,22 +6,23 @@ pandas2ri.activate()
 rMBC = importr("MBC")
 
 def MBCn(obs_dat,mod_dat,mod_nf,num_iter):
+    import time
+    start = time.time()
     '''Calculates for the Multivate Bias Correction using N-pdft of climate model outputs from MBC package by 
     Cannon (2018) <doi:10.1007/s00382-017-3580-6>'''
-   
+    
     #### Store input descriptions ####
     var_list,var_count = list(obs_dat.keys()),len(obs_dat.keys())
     shape = np.array(obs_dat[var_list[0]].shape)
     size = np.prod(shape)
-    
     #### Inputs must have the same lengths, so check for lengths ####
-    print('Checking if Datasets has equal legnths')
+    print('▮▮▮ Checking if Datasets have equal lengths.....')
     check1 = np.array_equal(np.array(obs[var_list[0]].shape),np.array(obs[var_list[0]].shape))
     check2 = np.array_equal(np.array(obs[var_list[0]].shape),np.array(hist[var_list[0]].shape))
     if check1 == True and check2 == True:
-        print('Datasets have the same dimension lengths, continue executing script')
+        print('▮▮▮ Datasets have the same dimension lengths, continue executing script.....')
     else:
-        print('Datasets does not have the same dimension lengths, abort executing script and Sayonara!')
+        print('▮▮▮ Datasets does not have the same dimension lengths, abort executing script and Sayonara!')
         import sys 
         sys.exit()
     ### Begin processing inputs and executing MBCn aglorith ####    
@@ -34,10 +35,8 @@ def MBCn(obs_dat,mod_dat,mod_nf,num_iter):
         dat2.append(joint_hist)
         dat3.append(joint_proj)
     obs1,hist1,proj1 = np.dstack(dat1).reshape(size,var_count),np.dstack(dat2).reshape(size,var_count),np.dstack(dat3).reshape(size,var_count)
-    
-    rf = rMBC.MBCn(obs1,hist1,proj1,num_iter,qmap_precalc=False,ratio_seq=np.repeat(False,var_count))[0]
-    nf = rMBC.MBCn(obs1,hist1,proj1,num_iter,qmap_precalc=False,ratio_seq=np.repeat(False,var_count))[1]      
-    model_rf,model_nf = np.hsplit(rf,var_count),np.hsplit(nf,var_count)
+    rf = rMBC.MBCn(obs1,hist1,proj1,num_iter,qmap_precalc=False,ratio_seq=np.repeat(False,var_count))
+    model_rf,model_nf = np.hsplit(rf[0],var_count),np.hsplit(rf[1],var_count)
     
     ref,pred = {},{}
     for i in range(var_count):
@@ -50,4 +49,8 @@ def MBCn(obs_dat,mod_dat,mod_nf,num_iter):
     for j,k in enumerate(var_list):
         oc[k] = (oc[k]*0+1)*ref[lists1[j]]
         pc[k] = (pc[k]*0+1)*pred[lists2[j]]
+        
+    end = time.time()
+    print("▮▮▮ Elapsed time in real time :" , time.strftime("%m",time.gmtime(end-start)),"minutes ▮▮▮")
+    
     return(oc,pc)
