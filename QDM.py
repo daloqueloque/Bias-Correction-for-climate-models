bde67b29d378af2979897c6b5fd2d3575237bd33
Dani Loqueloque <79607002+DarkLynx56@users.noreply.github.com>
2021-02-28 17:33:46 +0800

    Update QDM.py

--- QDM.py
+++ QDM.py
@@ -13,13 +13,12 @@ def QDM_proj(obs_dat,mod_dat,mod_nf):
     qdm = rMBC.QDM(obs_dat,mod_dat,mod_nf,ratio=False)[1]
     return(qdm)
 def apply_QDM(obs_dat,mod_dat,mod_nf):
-    var_list = list(obs_dat.keys())
     time1 = mod_dat['time'].values
     time2 = mod_nf['time'].values
-    hist = xr.apply_ufunc(QDM_hist, obs_dat[var_list[0]].drop('time'), mod_dat[var_list[0]].drop('time'), mod_nf[var_list[0]].drop('time'),
+    hist = xr.apply_ufunc(QDM_hist, obs_dat.drop('time'), mod_dat.drop('time'), mod_nf.drop('time'),
                       input_core_dims=[['time'],['time'],['time']],
                       output_core_dims=[['time']], vectorize=True)
-    proj = xr.apply_ufunc(QDM_proj, obs_dat[var_list[0]].drop('time'), mod_dat[var_list[0]].drop('time'), mod_nf[var_list[0]].drop('time'),
+    proj = xr.apply_ufunc(QDM_proj, obs_dat.drop('time'), mod_dat.drop('time'), mod_nf.drop('time'),
                       input_core_dims=[['time'],['time'],['time']],
                       output_core_dims=[['time']], vectorize=True)
     oc = hist.assign_coords({'time':time1}).to_dataset()
